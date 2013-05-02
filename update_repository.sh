edb18bceba298ef14572da8f83e3e1300b293690
Pierre-Alexis de Solminihac <pa@quai13.com>
2013-05-02 17:51:02 +0200

    MAJ devtools     corrections mineures dans create_package.sh     ajout du script update_repository,sh, permettant de creer un depot     en utilisant le format "a la github" sans avoir a passer par     github (trop instable et trop lent pour cette utilisation)

---
+++ update_repository.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+CLEMENTINE_REPOSITORY_URL="https://github.com/pa-de-solminihac"
+PAUSE_TIME=2
+
+# recupere la liste des modules dispo dans le dossier ../modules/
+MODULES=`ls -d ../modules/*/trunk/.git`
+
+# TODO: faire prendre en compte les arguments suivants
+#--package           : ne recupere que le package X
+#--version           : ne recupere que la version N.m du package X
+#--only-scripts      : ne recupere que les scripts
+#--only-packages     : ne recupere que les packages
+#--repository-url    : utilise le repository X
+
+# on cree le dossier repository s'il n'existe pas deja et on entre dedans
+echo
+echo "Creating whole repository"
+mkdir -p ../repository
+cd ../repository
+
+echo
+echo "Getting packages scripts"
+for MODULE_PATH in ${MODULES[@]}
+do
+    MODULE="${MODULE_PATH:11:-11}";
+    # recupere les scripts du module
+    mkdir -p clementine-framework-module-$MODULE-scripts/archive;
+    wget $CLEMENTINE_REPOSITORY_URL/clementine-framework-module-$MODULE-scripts/archive/master.zip -O clementine-framework-module-$MODULE-scripts/archive/master.zip;
+    # soyons cool avec github
+    sleep $PAUSE_TIME;
+done
+
+echo
+echo "Getting packages versions"
+for MODULE_PATH in ${MODULES[@]}
+do
+    MODULE="${MODULE_PATH:11:-11}";
+    mkdir -p clementine-framework-module-$MODULE/archive;
+    # recupere toutes les versions dispo du module
+    VERSIONS_DISPO=$(zip --show-files clementine-framework-module-$MODULE-scripts/archive/master.zip | grep "/versions/." | cut -d"/" -f 3 | sort -V | uniq)
+    for VERSION in ${VERSIONS_DISPO[@]};
+    do
+        wget $CLEMENTINE_REPOSITORY_URL/clementine-framework-module-$MODULE/archive/$VERSION.zip -O clementine-framework-module-$MODULE/archive/$VERSION.zip;
+        # soyons cool avec github
+        sleep $PAUSE_TIME;
+    done;
+done
+
+# retour au repertoire d'origine
+cd ../devtools
