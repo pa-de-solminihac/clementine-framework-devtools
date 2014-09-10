c1f57649f40cebe6b859668e727b56e5f92a7b75
Pierre-Alexis de Solminihac <pa@quai13.com>
2014-09-10 12:22:52 +0200

    Colored output

--- st.sh
+++ st.sh
@@ -2,19 +2,19 @@
 path_to_devtools="$(dirname "$0")"
 pushd $path_to_devtools > /dev/null
 echo "Status DEVTOOLS"
-(git status -sb | grep -v '## master')
+(git -c color.ui=always status -sb | grep -v '## ')
 
 echo
 echo "Status GIT-HOOKS"
-(cd ../git-hooks && git status -sb | grep -v '## master')
+(cd ../git-hooks && git -c color.ui=always status -sb | grep -v '## ')
 
 echo
 echo "Status INSTALLER"
-(cd ../www/install && git status -sb | grep -v '## master')
+(cd ../www/install && git -c color.ui=always status -sb | grep -v '## ')
 
 echo
 echo "Status WWW"
-(cd ../www/trunk && git status -sb | grep -v '## master')
+(cd ../www/trunk && git -c color.ui=always status -sb | grep -v '## ')
 
 echo
 echo "Status modules"
@@ -22,11 +22,10 @@ g=`ls -d ../modules/*/trunk/.git`
 for repo in ${g[@]}
 do
     module_name="$(echo "$repo" | cut -d "/" -f 3)"
-    echo
-    echo " - $module_name"
-    (cd $repo && cd ../../trunk && git status -sb | grep -v '## master')
-    echo " - $module_name (scripts)" 
-    (cd $repo && cd ../../repository/scripts && git status -sb | grep -v '## master')
+    echo "  $module_name"
+    (cd $repo && cd ../../trunk && git -c color.ui=always status -sb | grep -v '## ' | sed 's/^/    /g')
+    # echo " - $module_name (scripts)" 
+    (cd $repo && cd ../../repository/scripts && git -c color.ui=always status -sb | grep -v '## ' | sed 's/^/    /g')
 done
 
 popd > /dev/null
