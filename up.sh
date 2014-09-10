a698a3e10bdd0a36a650a01683cbfefc60afc140
Pierre-Alexis de Solminihac <pa@quai13.com>
2014-09-10 11:57:29 +0200

    Parallelized modules updates in up.sh, with GNU-Parallel

--- up.sh
+++ up.sh
@@ -16,16 +16,32 @@ echo
 echo "Update WWW"
 (cd ../www/trunk && git pull | grep -v "Already up-to-date" && git submodule update && git submodule foreach git pull | grep -v "Already up-to-date")
 
+# detecte si GNU Paralllel est disponible
+# O. Tange (2011): GNU Parallel - The Command-Line Power Tool,
+# ;login: The USENIX Magazine, February 2011:42-47.
+which parallel > /dev/null
+NOPARALLEL=$?
+
 echo
 echo "Update modules"
 g=`ls -d ../modules/*/trunk/.git`
-for repo in ${g[@]}
-do
-    echo -n " - "
-    echo "$repo" | cut -d"/" -f 3
-    (cd ${repo} && cd ../../trunk && git pull | grep -v "Already up-to-date")
-    (cd ${repo} && cd ../../repository/scripts && git pull | grep -v "Already up-to-date")
-done
+if [[ $NOPARALLEL > 0 ]];
+then
+    for repo in ${g[@]}
+    do
+        echo -n " - "
+        echo "$repo" | cut -d"/" -f 3
+        (cd ${repo} && cd ../../trunk && git pull | grep -v "Already up-to-date")
+        (cd ${repo} && cd ../../repository/scripts && git pull | grep -v "Already up-to-date")
+    done
+else
+    echo "${g[@]}" | parallel --gnu -j8 '
+        echo -n " - "
+        echo "{}" | cut -d"/" -f 3
+        (cd {} && cd ../../trunk && git pull | grep -v "Already up-to-date")
+        (cd {} && cd ../../repository/scripts && git pull | grep -v "Already up-to-date")
+    '
+fi
 
 echo
 echo "--------------------------------------------------"
