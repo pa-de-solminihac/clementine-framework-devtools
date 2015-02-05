0c3596da32e3d4fb82a7e68bd70c8dd56b7ac7c2
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-02-05 17:19:46 +0100

    controle d'integrite : on verifie que la version renseignée dans modules.ini est bien celle du package qu'on veut créer recuperation du dernier message de commit du module pour faire l'annotation du tag

--- create_package.sh
+++ create_package.sh
@@ -17,12 +17,13 @@ EOF
     exit 0
 }
 
-# help
+#help
 if [[ "$#" != 2 && "$#" != 3 ]]
 then
     usage
 fi
 
+#parameters
 PACKAGE=$1
 VERSION=$2
 SRC="trunk"
@@ -30,53 +31,66 @@ if [[ "$#" == 3 ]]
 then
     SRC=$3
 fi
+
+#integrity check
+if [[ "version=$VERSION" != "$(grep -o 'version=.*' ../modules/$PACKAGE/$SRC/etc/module.ini)" ]]
+then
+    echo "Version mismatch in module.ini !"
+    exit 1
+fi
+
 cd ../modules/$PACKAGE && \
-    # cp -pr $SRC $PACKAGE && \
-    # zip -r $PACKAGE-$VERSION.zip $PACKAGE -x *.svn* *.git* && \
-    # rm -rf $PACKAGE && \
-    # mv $PACKAGE-$VERSION.zip repository/src/ && \
+    #cp -pr $SRC $PACKAGE && \
+    #zip -r $PACKAGE-$VERSION.zip $PACKAGE -x *.svn* *.git* && \
+    #rm -rf $PACKAGE && \
+    #mv $PACKAGE-$VERSION.zip repository/src/ && \
     cd repository/scripts && \
     mkdir -p versions/$VERSION && \
     touch versions/$VERSION/.gitignore && \
-    # rm -rf scripts.zip && \
-    # zip -r scripts.zip scripts -x *.svn* *.git* && \
+    #rm -rf scripts.zip && \
+    #zip -r scripts.zip scripts -x *.svn* *.git* && \
     git add versions/$VERSION
     git add versions/$VERSION/.gitignore
     git add versions/$VERSION/*
     git commit -m "$PACKAGE $VERSION"
     cd ../../trunk
 
-# TODO: mettre à jour le fichier module/.../repository/scripts.zip du repository avec les dependances pour cette version si nécessaire
-# rappel : le fichier module/repository/scripts.zip est destiné au repository, et contient toutes les dépendances pour toutes les versions du module
-# cd ../devtools && ./update_package_scripts.sh $PACKAGE $VERSION
+#TODO: mettre à jour le fichier module/.../repository/scripts.zip du repository avec les dependances pour cette version si nécessaire
+#rappel : le fichier module/repository/scripts.zip est destiné au repository, et contient toutes les dépendances pour toutes les versions du module
+#cd ../devtools && ./update_package_scripts.sh $PACKAGE $VERSION
 
 cat <<EOF
 
-# Penser à lancer up.sh afin de récupérer les paquets à jour !
+#Penser à lancer up.sh afin de récupérer les paquets à jour !
 
-# C'est presque prêt, il faut maintenant lancer les commandes suivante :
+#C'est presque prêt, il faut maintenant lancer les commandes ci-dessous,
+#en mettant un message de commit de la forme :
+#MODULE N.m
+#
+#par exemple :
+#
+#USERS 4.9
+#Amélioration de la procédure de renouvellement de mot de passe
+#Messages plus clairs, meilleure cinématique, alerte l'utilisateur si son compte est désactivé.
 
 cd ../modules/$PACKAGE/trunk
+git pull
 git commit -a
 
-# mettre un message de commit de la forme :
-# MODULE N.m
-#
-# par exemple :
-#
-# USERS 4.9
-# Amélioration de la procédure de renouvellement de mot de passe
-# Messages plus clairs, meilleure cinématique, alerte l'utilisateur si son compte est désactivé.
+#================================================
+# C'est le moment d'écrire le message de commit !
+#================================================
 
-git tag -a $VERSION -m "version $VERSION"
+git tag -a $VERSION -m "\$(git log -1 --pretty=%B)"
 git push
 git push --tags
 cd ../repository/scripts
 git push
 cd ../../../../devtools
 
-# Il faut maintenant aller mettre à jour le dépot sur clementine.quai13.com :
-# on se connecte en SSH
+#Il faut maintenant aller mettre à jour le dépot sur clementine.quai13.com :
+#on se connecte en SSH et on lance :
+
 cd www/devtools
 git pull; ./update_repository.sh
 
