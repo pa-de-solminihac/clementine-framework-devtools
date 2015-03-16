36f8a1de2e9d05f2d1d1f1fb19ba7dc587e50e25
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-03-16 11:50:57 +0100

    Modules supplémentaires, et script import_missing_modules.sh pout importer les modules manquants depuis leurs depôts github

---
+++ import_missing_modules.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+GREEN=`echo -e '\033[0m\033[1;32m'`
+NORMAL=`echo -e '\033[0m'`
+MODULES="$(cat modules.list)"
+for MODULE in ${MODULES[@]}
+do
+    mkdir -p ../modules/$MODULE/trunk
+    mkdir -p ../modules/$MODULE/repository/scripts
+
+    if [ ! -d "../modules/$MODULE/trunk/.git" ]; 
+    then
+        echo "${GREEN}Importing ${MODULE}${NORMAL}"
+        cd ../modules/$MODULE/trunk/
+        git clone git@github.com:pa-de-solminihac/clementine-framework-module-${MODULE}.git . || "You should create the repository pa-de-solminihac/clementine-framework-module-${MODULE}"
+        cd -
+    fi
+
+    if [ ! -d "../modules/$MODULE/repository/scripts/.git" ]; 
+    then
+        echo "${GREEN}Importing ${MODULE} scripts ${NORMAL}"
+        cd ../modules/$MODULE/repository/scripts
+        #git init
+        #git add depends.ini versions
+        #git commit -m "$MODULE 1.0 scripts"
+        #git remote add origin git@github.com:pa-de-solminihac/clementine-framework-module-${MODULE}-scripts.git
+        git clone git@github.com:pa-de-solminihac/clementine-framework-module-${MODULE}-scripts.git . || "You should create the repository git@github.com:pa-de-solminihac/clementine-framework-module-${MODULE}-scripts.git"
+        cd -
+    fi
+
+    # on remplit avec les éléments de base APRES avoir cloné
+    mkdir -p ../modules/$MODULE/repository/scripts/versions
+    touch ../modules/$MODULE/repository/scripts/depends.ini
+
+done
