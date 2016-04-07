6c030028931caf24f07d16bb88b70f070b295143
Pierre-Alexis de Solminihac <pa@quai13.com>
2016-04-07 18:15:38 +0200

    chore(release): update create_relase script

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+SORT="$(which gsort)"
+if [ ! -x $SORT ];
+then
+    SORT="sort"
+fi
+
 usage() {
 
     echo "Usage : `basename $0` <version>"
@@ -23,33 +29,36 @@ fi
 
 VERSION=$1
 # le module core sera installé par l'installeur, qui choisira la bonne version
-COREVERSION="`ls ../modules/core/repository/src/core-*.zip | sort -V | tail -n 1 | sed 's/.*\/core-//g' | sed 's/.zip//g'`"
+COREVERSION="`ls ../repository/clementine-framework-module-core/archive/*.zip | $SORT -V | tail -n 1 | sed 's/.*\///g' | sed 's/.zip//g'`"
 cd ../releases && \
     cp -pr ../www/trunk clementine-$VERSION && \
-    rm -rf clementine-$VERSION/app && \
+    #rm -rf clementine-$VERSION/app && \
     mkdir -p clementine-$VERSION/app/share && \
-    mkdir -p clementine-$VERSION/app/local/site && \
     mkdir -p clementine-$VERSION/app/local/site/etc && \
     # cp -pr ../modules/core/repository/src/core-$COREVERSION.zip clementine-$VERSION/app/share/ && \
-    unzip -p ../modules/core/repository/src/core-$COREVERSION.zip core/etc/config.ini > clementine-$VERSION/app/local/site/etc/config.ini && \
+    # unzip -p ../repository/clementine-framework-module-core/archive/$COREVERSION.zip clementine-framework-module-core-$COREVERSION/etc/config.ini > clementine-$VERSION/app/local/site/etc/config.ini && \
     # rm clementine-$VERSION/app/share/core-$COREVERSION.zip && \
     # cp -pr clementine-$VERSION/app/share/core/etc/config.ini clementine-$VERSION/app/local/site/etc/config.ini
     # mkdir -p clementine-$VERSION/app/local/site/model && \
     # mkdir -p clementine-$VERSION/app/local/site/view && \
     # mkdir -p clementine-$VERSION/app/local/site/ctrl && \
     # mkdir -p clementine-$VERSION/app/local/site/skin && \
+    rm -rf clementine-$VERSION/README.md && \
+    rm -rf clementine-$VERSION/install/README.md && \
+    cp -p ../www/trunk/.htaccess clementine-$VERSION && \
     rm -rf clementine-$VERSION/tmp && \
     mkdir -p clementine-$VERSION/tmp && \
     cp -p ../www/trunk/tmp/.htaccess clementine-$VERSION/tmp && \
     rm -rf clementine-$VERSION/install && \
     cp -pr ../www/install clementine-$VERSION && \
+    rm -rf clementine-$VERSION/install/README.md && \
     rm -rf clementine-$VERSION/install/config.php && \
     rm -rf clementine-$VERSION/install/tmp && \
     rm -rf clementine-$VERSION.zip && \
-    zip -r clementine-$VERSION.zip clementine-$VERSION -x *.svn* && \
+    zip -r clementine-$VERSION.zip clementine-$VERSION -x *.git* && \
     rm -rf clementine-$VERSION && \
     cat <<EOF
 
-Nouveau fichier créé : /clementine/releases/clementine-$VERSION.zip
+Nouveau fichier créé : ../releases/clementine-$VERSION.zip
 
 EOF
